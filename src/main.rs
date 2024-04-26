c1454c179afdcd644d5bcdd6522d8ff606766071
Eoghan Hynes <eoghan@hyn.es>
2024-04-26 16:14:18 +0100

    Use one component

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 
 use dioxus::prelude::*;
 use log::LevelFilter;
-use ndarray::prelude::*;
 
 fn main() {
     dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
@@ -13,26 +12,18 @@ fn main() {
 #[component]
 fn App() -> Element {
     let rowLength = 100;
-    let matrix = vec![false; rowLength * rowLength];
+    let mut matrix = use_signal(|| vec![false; rowLength * rowLength]);
 
     rsx! {
         link { rel: "stylesheet", href: "main.css" }
         div { class: "main", padding: "0.5rem", position: "relative",
             div { class: "container", max_width: "{rowLength*10}px",
-                 for (i, item) in matrix.iter().enumerate() {
-                     Square {}
+                 for (i, &item) in matrix().iter().enumerate() {
+                    div { onclick: move |_| {
+                        matrix.write()[i] = !item;
+                    }, class: if item { "square active" } else { "square" } }
                  }
             }
         }
-    }
-}
-
-#[component]
-fn Square() -> Element {
-    let mut active = use_signal(|| false);
-    rsx! {
-        div { onclick: move |_| {
-            active.set(if active() { false } else { true });
-        }, class: if active() { "square active" } else { "square" } }
     }
 }
